49b72f362131c266146a64bc9942694728bf0cb1
ahmedsawy1 <sawy22417@gmail.com>
2025-09-11 17:05:01 +0300

    finish delete

--- routes/category.route.js
+++ routes/category.route.js
@@ -5,11 +5,10 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
   try {
-
-    if ( !req.body.name || req.body.name.trim().length < 3) {
-        return res.status(400).send({
-            message: req.t("categoryNameValidation")
-        })
+    if (!req.body.name || req.body.name.trim().length < 3) {
+      return res.status(400).send({
+        message: req.t("categoryNameValidation"),
+      });
     }
     const newCategory = await Category.create({
       name: req.body.name,
@@ -20,5 +19,31 @@ router.post("/", async (req, res) => {
     return res.status(400).send({ message: err.message });
   }
 });
+
+router.get("/", async (req, res) => {
+  try {
+    const categoriesList = await Category.find();
+    if (!categoriesList || categoriesList.length === 0) {
+      return res.send({message: "noCategories"})
+    }
+    res.send(categoriesList);
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+router.delete("/:id", async(req, res) => {
+  try{
+    const catg = await Category.findByIdAndDelete(req.params.id)
+    if(!catg) {
+      return res.status(404).send({message: req.t("categoryNotFound")})
+    }
+
+    return res.send({message: req.t("categoryDeletedSuccessfully")})
+  }
+  catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+})
 
 module.exports = router;
