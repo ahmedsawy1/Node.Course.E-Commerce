77eec01134c23319bea3b3e45646757879254261
ahmedsawy1 <sawy22417@gmail.com>
2025-09-13 16:06:27 +0300

    finish hashing

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const userSchema = new mongoose.Schema(
   {
@@ -60,6 +61,22 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// Hashing password before saving.
+userSchema.pre("save", async function(next) {
+    // skip hashing if password wasn't modified 
+    if (!this.isModified("password")) return next()
+
+    try {
+        const salt = await bcrypt.genSalt(10)
+
+        this.password = await bcrypt.hash(this.password, salt)
+
+        next()
+    } catch (error) {
+        next(error)
+    }  
+})
 
 const User = mongoose.model("User", userSchema);
 export default User;
