46149d8dee3f493e3755ad374d3c31f80369ce7a
ahmedsawy1 <sawy22417@gmail.com>
2025-09-13 15:55:08 +0300

    adding addresses

--- models/user.model.js
+++ models/user.model.js
@@ -1,35 +1,65 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
-    {
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-
-        password: {
-            type: String,
-            required: true,
-            minlength: 6,
-        },
-
-        role: {
-            type: String,
-            enum: ["admin", "user"],
-            default: "user"
-        },
-
-        userName:{
-            type: String,
-            required: true,
-            trim: true
-        }
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    {
-        timestamps: true
-    }
-)
 
-const User = mongoose.model("User", userSchema)
-export default User
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+
+    userName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    postalCode: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    addressLine1: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    addressLine2: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    phoneNumber: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
