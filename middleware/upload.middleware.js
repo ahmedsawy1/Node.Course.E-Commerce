c040b0bb3d9650346ea96566198b5d554c424a58
ahmedsawy1 <sawy22417@gmail.com>
2025-09-20 13:49:06 +0300

    finish multer

---
+++ middleware/upload.middleware.js
@@ -0,0 +1,44 @@
+import multer from "multer";
+import path from "path"
+import fs from "fs"
+
+const UPLOAD_DIR = "public/uploads"
+
+if (!fs.existsSync(UPLOAD_DIR)) {
+    fs.mkdirSync(UPLOAD_DIR, {recursive: true})
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, UPLOAD_DIR)
+    },
+
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.round() * 1e9)
+
+        const extension = path.extname(file.originalname);
+
+        cb(null, `product-${uniqueSuffix}${extension}`)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype.stateWith("image/")) {
+        cb(null, true)
+    } else {
+        cb(new Error("Only images files are allowed"), false)
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5 MB limit for file
+        files: 10
+    }
+})
+
+export const uploadSingle = upload.single("image")
+
+export const uploadMultiple = upload.array("images", 10)
