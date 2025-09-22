6f56ccb1b9d540959fe8fc8eb9814ee6b010bfa5
ahmedsawy1 <sawy22417@gmail.com>
2025-09-22 21:39:17 +0300

    tasks

---
+++ Tasks.md
@@ -0,0 +1,5 @@
+1- Make Validation For Post Product
+
+2- Make Get Product Route & Request in Postman
+
+3- Add Localization Keys in all languages ( 😂 Don't worry I will help you 😂 )
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,34 +1,61 @@
 import express from "express";
 import { ProductModel } from "../models/product.model.js";
 import { handleRouteError } from "../helpers/error-handling.js";
-import { getFileURL, handleUploadError, uploadMultiple } from "../middleware/upload.middleware.js";
-import { adminOnly } from "../middleware/roles.middleware.js";
+import {
+  getFileURL,
+  handleUploadError,
+  uploadMultiple,
+} from "../middleware/upload.middleware.js";
+import { adminOnly, userAndAdmin } from "../middleware/roles.middleware.js";
+import {
+  createProductValidation,
+  handleValidationErrors,
+} from "../validators/product.validator.js";
 
 const router = express.Router();
 
-router.post("/", adminOnly, uploadMultiple, handleUploadError,async (req, res) => {
-  try {
+router.post(
+  "/",
+  adminOnly,
+  uploadMultiple,
+  handleUploadError,
+  createProductValidation,
+  handleValidationErrors,
+  async (req, res) => {
+    try {
+      let imageURLs = [];
+      if (req.files && req.files.length > 0) {
+        imageURLs = req.files.map((file) => getFileURL(req, file.filename));
+      }
 
-    let imageURLs = []
-    if(req.files && req.files.length > 0) {
-      imageURLs = req.files.map((file) => getFileURL(req, file.filename))
+      let newProduct = new ProductModel({
+        title: req.body.title,
+        price: parseFloat(req.body.price), // "1.6" => 1.6
+        category: req.body.category,
+        countInStock: parseInt(req.body.countInStock), // "1" => 1
+        description: req.body.description,
+        images: imageURLs,
+      });
+
+      newProduct = await newProduct.save();
+
+      return res.status(201).json({
+        success: true,
+        message: req.t("productCreatedSuccessfully"),
+        data: newProduct,
+      });
+    } catch (error) {
+      handleRouteError(error, res);
     }
-    
-    let newProduct = new ProductModel({
-      title: req.body.title,
-      price: parseFloat(req.body.price), // "1.6" => 1.6
-      category: req.body.category,
-      countInStock: parseInt(req.body.countInStock), // "1" => 1
-      description: req.body.description,
-      images: imageURLs
-    });
+  }
+);
 
-    newProduct = await newProduct.save();
+router.get("/", userAndAdmin, async (req, res) => {
+  try {
+    const productsList = await ProductModel.find().populate("category", "name");
 
-    return res.status(201).json({
-      success: true,
-      message: req.t("productCreatedSuccessfully"),
-      data: newProduct,
+    res.send({
+      data: productsList,
     });
   } catch (error) {
     handleRouteError(error, res);
